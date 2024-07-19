36c6784800a387c791010b9cbbdc977bccef212d
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-19 11:30:44 -0400

    1

---
+++ src/AdultUser.java
@@ -0,0 +1,4 @@
+public class AdultUser implements LibraryUser {
+    int age;
+    String bookType;
+}
---
+++ src/KidUser.java
@@ -0,0 +1,11 @@
+public class KidUser implements LibraryUser{
+    int age;
+    String bookType;
+}
+
+
+public void registerAccount(int age){
+    if(age <= 11){
+        System.out.println("You have successfully registered under a Kids Account");
+        }
+        }
